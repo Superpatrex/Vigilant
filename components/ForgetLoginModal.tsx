258877a5f292a4843567de88a2184194ec4cc7a7
TomatoSopa <sophiad03@hotmail.com>
2023-04-18 04:03:07 -0400

    Forgot Login

--- components/ForgetLoginModal.tsx
+++ components/ForgetLoginModal.tsx
@@ -7,18 +7,50 @@ import {
   } from 'react-native';
 import { useTheme } from '@react-navigation/native';
 import { TextInput } from 'react-native-gesture-handler';
-
+import buildPath from '../buildPath';
 
 const ForgetLoginModal = ({ navigation }) => {
     const { colors } = useTheme();
-
     const [email, setEmail] = React.useState('');
+
+    const Reset = async (email: string) => {
+        var js = JSON.stringify(email);
+        const [errorVisible, setErrorVisible] = React.useState(false);
+        const [errorMessage, setErrorMessage] = React.useState('');
+        
+        try
+        {
+            const response = await fetch(buildPath('api/resetPassword'),
+            {method: 'POST', body: js, headers: {'Content-Type': 'application/json'}});
+        
+            var res = JSON.parse(await response.text());
+        
+            console.log(res);
+            if (res.success)
+            {
+                // Alert.alert('User created');
+                setErrorMessage('Sent email');
+                setErrorVisible(true);
+            }
+            else
+            {
+                setErrorMessage(res.error);
+                setErrorVisible(true);
+            }
+        }
+        catch (e: any)
+        {
+            throw e;
+        }
+
+        navigation.back()
+    }
 
     return (
         <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
             <Text style={{ fontSize: 18, color: colors.text }}>Enter your email and we'll send you a link</Text>
             <TextInput style={styles.input} autoCorrect={false} autoCapitalize='none' autoComplete='email' onChangeText={text => setEmail(text)} placeholder='Enter your email here' placeholderTextColor={"#6b6b6b"}></TextInput>
-            <Button onPress={() => navigation.goBack()} title="Send"></Button>
+            <Button onPress={() => Reset(email)} title="Send"></Button>
         </View>
     );
 }
