690ac47e03fb775ed3752e172e19d5a195798a22
Bostonian02 <bostonian02@gmail.com>
2023-04-18 16:10:59 -0400

    Fix password error

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -102,7 +102,7 @@ function Login()
     //      setMessage('Passwords do not match');
     //      document.getElementById("registerResult").style.setProperty("opacity", 1);
     // }
-    if (!validatePassword.test(password))
+    if (!validatePassword.test(registerPassword.value))
     {
         setRegisterMessage('Password does not meet requirements');
         document.getElementById("registerResult").style.setProperty("opacity", 1);
